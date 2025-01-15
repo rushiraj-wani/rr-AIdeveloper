6c4aea59b9f7b3298de36f5bda229e6abb96c0f6
Rushiraj Wani <rajwani2003@gmail.com>
2025-01-15 20:58:51 +0530

    Completed Login and Registration Integration

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -14,7 +14,7 @@ export const createUserController = async (req, res) => {
 
     const token = await user.generateJWT();
 
-    // delete user._doc.password;
+    delete user._doc.password;
 
     res.status(201).json({ user, token });
   } catch (error) {
@@ -50,7 +50,7 @@ export const loginController = async (req, res) => {
 
     const token = await user.generateJWT();
 
-    // delete user._doc.password;
+    delete user._doc.password;
 
     res.status(200).json({ user, token });
   } catch (err) {
