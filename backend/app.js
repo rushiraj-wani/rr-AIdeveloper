433e09b9241846af7ac68e0ae4e0fce9324d3aad
Rushiraj Wani <rajwani2003@gmail.com>
2025-01-16 17:30:19 +0530

    Setup AI

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import connect from "./db/db.js";
 import userRoutes from "./routes/user.routes.js";
 import projectRoutes from "./routes/project.routes.js";
+import rrRoutes from "./routes/rr.routes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
@@ -18,6 +19,7 @@ app.use(cookieParser());
 
 app.use("/users", userRoutes);
 app.use("/projects", projectRoutes);
+app.use("/ai", rrRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
